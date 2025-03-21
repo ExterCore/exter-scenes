4637e7a8a0f1568def7b2e9f2aa1ae5b1f1dccf4
CtrlAltDelete4413 <95325881+CtrlAltDelete4413@users.noreply.github.com>
2025-03-21 19:20:11 +0700

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version "cerulean"
+game "gta5"
+lua54 "yes"
+
+author "sobing"
+
+description "EXTER-SCENE NOPIXEL DESIGN 4.0"
+
+version '1.0'
+
+ui_page 'web/build/index.html'
+
+client_script "client/**/*"
+
+server_script "server/**/*"
+
+files {
+	'web/build/index.html',
+	'web/build/**/*',
+}
